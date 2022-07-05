ee13b6d1b94488eb97fa809f7b3599bda4bf25f4
Subhendra Basu <basu.subhendra@outlook.com>
2022-07-06 03:47:02 +0530

    Completed first level of coding

--- web_crawler/src/test/scala/MySuite.scala
+++ web_crawler/src/test/scala/MySuite.scala
@@ -1,9 +1,38 @@
-// For more information on writing tests, see
-// https://scalameta.org/munit/docs/getting-started.html
-class MySuite extends munit.FunSuite {
-  test("example test that succeeds") {
-    val obtained = 42
-    val expected = 42
-    assertEquals(obtained, expected)
+class WebCrawlerSuite extends WordSpec with MockFactory  {
+  trait HyperLinksFixture {
+    val connection = mock[Connection]
+    val getConnection = mockFunction[String, Connection]
+    lazy val web_crawler = new WebCrawler() {
+      override def getConnection(url_to_be_processed: String) = 
+        HyperLinksFixture.this.getConnection(url_to_be_processed) 
+    }
+  }
+  trait TraverseFixture {
+    val getHyperLinksPage = mock[String, Option[List[String]]]
+    lazy val web_crawler = new Web_Crawler() {
+      override def getHyperLinksPage(url_to_be_processed: String) =
+        TraverseFixture.this.getHyperLinksPage(url_to_be_processed)
+    }
+  }
+
+  "get_sub_links" should {
+      "return the links" in new HyperLinksFixture {
+        val url_to_be_processed = "https://www.wikipedia.org/"
+        getConnection expects(url_to_be_processed) returning connection
+        web_crawler.get_sub_links(url_to_be_processed) shouldBe expected
+      }
+  }
+
+  "traverse" should {
+    "traverse over the hyperlinks" in new TraverseFixture {
+      get_sub_links expects(*) onCall {
+        _ match {
+          case "a" => Some(List("b", "c"))
+          case "b" => Some(List("d"))
+          case _ => None
+        }
+      }
+      web_crawler.traverse(Some(List("a")), List.empty[String]) shouldBe
+    }
   }
 }
